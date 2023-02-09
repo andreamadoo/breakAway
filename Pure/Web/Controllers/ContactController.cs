062eb2a970c22f8cb5c1ec3553c487e12a67c848
andre.amado <andre.amado@keg.com>
2023-02-09 15:54:04 +0100

    First 3 tasks

---
+++ Pure/Web/Controllers/ContactController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web.Mvc;
+using BreakAway.Entities;
+using BreakAway.Models.Contact;
+
+namespace BreakAway.Controllers
+{
+
+    public class ContactController : Controller
+    {
+        private readonly IRepository _repository;
+
+        public ContactController(IRepository repository)
+        {
+            if (repository == null)
+            {
+                throw new ArgumentNullException("repository");
+            }
+
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult Index(string message, string firstName, string lastName, string addDate, string modifiedDate)
+        {
+            if (!string.IsNullOrEmpty(message))
+            {
+                ViewBag.message = message;
+            }
+
+            var viewModel = new IndexViewModel();
+
+            ViewData["firstName"] = firstName;
+
+            ViewData["lastName"] = lastName;
+
+            ViewData["addDate"] = addDate;
+
+            ViewData["modifiedDate"] = modifiedDate;
+
+
+
+
+
+            viewModel.Contacts = (from contact in _repository.Contacts
+                                  select new ContactItem
+                                  {
+                                     
+                                      FirstName = contact.FirstName,
+                                      LastName = contact.LastName,
+                                      AddDate = contact.AddDate,
+                                      ModifiedDate = contact.ModifiedDate,
+                                      
+                                  }).ToArray();
+
+
+            if (!string.IsNullOrEmpty(firstName))
+            {
+
+                viewModel.Contacts = viewModel.Contacts.Where(i => i.FirstName.Contains(firstName)).ToArray();
+                
+            }
+
+            if (!string.IsNullOrEmpty(lastName))
+            {
+
+                viewModel.Contacts = viewModel.Contacts.Where(i => i.LastName.Contains(lastName)).ToArray();
+
+            }
+
+            if (!string.IsNullOrEmpty(addDate))
+            {
+
+                viewModel.Contacts = viewModel.Contacts.Where(i => i.AddDate.ToString().Contains(addDate)).ToArray();
+
+            }
+
+            if (!string.IsNullOrEmpty(modifiedDate))
+            {
+
+                viewModel.Contacts = viewModel.Contacts.Where(i => i.ModifiedDate.ToString().Contains(modifiedDate)).ToArray();
+
+            }
+
+
+
+
+            return View(viewModel);
+        }
+
+        
+    }
+
+   
+}
---
+++ Pure/Web/Models/Contact/IndexViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BreakAway.Entities;
+
+namespace BreakAway.Models.Contact
+{
+    public class IndexViewModel
+    {
+        public ContactItem[] Contacts { get; set; }
+    }
+
+    public class ContactItem
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime AddDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        
+    }
+}
